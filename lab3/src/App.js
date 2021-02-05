0663305b1dbb7cc7136191fdba58448d4cedcb19
Preston Foshee <prestonfoshee@knights.ucf.edu>
2021-02-05 08:36:54 -0500

    Lab 3

---
+++ lab3/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from './Components/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <HomePage firstName="Preston" />
+      </h1>
+    </div>
+  );
+}
+
+export default App;
